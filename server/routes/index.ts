4b624333748e98da04b5755a36b65f0d3a03bf3a
Siddhesh <iamsiddhesh22@gmail.com>
2025-12-02 13:47:30 +0530

    use auth middleware for all the rotes except for auth

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,6 +2,11 @@ import express from "express";
 import authRoutes from "./auth";
 import userRoutes from "./user";
 import adminRoutes from "./admin";
+import preferenceRoutes from "./preference";
+import matchRoutes from "./match";
+import likeRoutes from "./like";
+import chatRoutes from "./chat";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
@@ -9,7 +14,11 @@ router.get("/", (_, res) => {
   res.send("Hello World!");
 });
 router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
-router.use("/admin", adminRoutes);
+router.use("/user", authMiddleware, userRoutes);
+router.use("/admin", authMiddleware, adminRoutes);
+router.use("/preference", authMiddleware, preferenceRoutes);
+router.use("/match", authMiddleware, matchRoutes);
+router.use("/like", authMiddleware, likeRoutes);
+router.use("/chat", authMiddleware, chatRoutes);
 
 export default router;
